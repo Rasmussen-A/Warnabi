836a15fba55e99b5a09df10e77fc3fff353a2d12
Andrew <re.admit.re@gmail.com>
2014-01-18 19:51:55 +0400

    Initialize

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+ruby '1.8.7'
+
+gem 'rails', '3.2.0'
+
+group :development, :testing do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
+
+gem 'json'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warnabi::Application.config.secret_token = '98f8f0f0ecc20b0322d5656399c5f0d50f6e06250e27d451ed382a880e1dd33a648f8f71928c33963f14f93eff9888fa0618b50b156e9d63c529fa291af6172b'
